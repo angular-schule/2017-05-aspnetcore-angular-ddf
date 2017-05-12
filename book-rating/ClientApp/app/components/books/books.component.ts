22a2e8040e2e59405a9cfed8e9102f8db189a464
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-05-12 14:31:01 +0200

    property binding / @Input

--- book-rating/ClientApp/app/components/books/books.component.ts
+++ book-rating/ClientApp/app/components/books/books.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from "../../models/book";
 
 @Component({
   selector: 'br-books',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
+  books: Book[]; 
 
   constructor() { }
 
   ngOnInit() {
+    this.books = [      
+      new Book('000', 'Angular', 'Zurück in die Zukunft', 5),
+      new Book('111', 'AngularJS 1.x', 'Oldie but Goldie', 3)
+    ];
   }
-
 }
